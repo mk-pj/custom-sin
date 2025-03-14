017e40b3760976133ad8057918b678bd18ba618b
mk-pj <s31209@pjwstk.edu.pl>
2025-03-15 00:52:24 +0100

    first

---
+++ custom_sin.cpp
@@ -0,0 +1,153 @@
+#include <cmath>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <fmt/core.h>
+
+#define FIRST_QUARTER_END (M_PI / 2)
+#define SECOND_QUARTER_END M_PI
+#define THIRD_QUARTER_END (3 * M_PI / 2)
+#define _2PI (2 * M_PI)
+
+using std::vector;
+
+double custom_sin(double x, const int term_count, const bool is_deg = false) {
+  if (is_deg)
+    x *= M_PI / 180;
+
+  double norm_x = x - _2PI * trunc(x / _2PI);
+
+  if (norm_x < 0)
+    norm_x += _2PI;
+
+  bool is_negative = false;
+
+  if (norm_x > THIRD_QUARTER_END) {
+    norm_x = _2PI - norm_x;
+    is_negative = true;
+  } else if (norm_x > SECOND_QUARTER_END) {
+    norm_x = norm_x - M_PI;
+    is_negative = true;
+  } else if (norm_x > FIRST_QUARTER_END) {
+    norm_x = M_PI - norm_x;
+  }
+
+  double sin_x = 0.0;
+  double term = norm_x;
+
+  for (int i = 1, sign = 1; i <= term_count; ++i, sign *= -1) {
+    sin_x += sign * term;
+    term *= norm_x * norm_x / ((2 * i) * (2 * i + 1));
+  }
+
+  if (is_negative)
+    sin_x *= -1;
+
+  return sin_x;
+}
+
+void rad_comparison(
+    const int n,
+    const int term_count,
+    const double upper_bound,
+    const std::string &title
+) {
+  double x = -upper_bound;
+  double step = 2 * upper_bound / (n - 1);
+  vector<double> X, Y, Y_calc;
+
+  X.reserve(n);
+  Y.reserve(n);
+  Y_calc.reserve(n);
+
+  for (int _ = 0; _ < n; ++_) {
+    double sin_h = sin(x);
+    double cust_sin = custom_sin(x, term_count);
+    double error = fabs(sin_h - cust_sin);
+    X.push_back(x);
+    Y.push_back(sin_h);
+    Y_calc.push_back(cust_sin);
+    fmt::println("{:=^80}", "");
+    fmt::println("{:^10.2f}|{:^20.10f}|{:^25.10f}|{:^30.10f}", x, sin_h, cust_sin, error);
+    x += step;
+  }
+
+  std::stringstream ss;
+  for (int i = 0; i < X.size(); ++i) {
+    double error = fabs(Y_calc[i] - Y[i]);
+    ss << X[i] << ',' << Y_calc[i] << ',' << Y[i] << ',' << error << '\n';
+  }
+
+  std::filesystem::create_directory("data_output/");
+  std::fstream rad("data_output/" + title, std::ios::out | std::ios::trunc);
+  if (!rad) {
+    std::cerr << "Failed to write to the file\n";
+    return;
+  }
+  rad << ss.str();
+  rad.close();
+}
+
+void deg_comparison(
+    const int n,
+    const int term_count,
+    const double upper_bound,
+    const std::string &title
+) {
+  double x = -upper_bound;
+  double step = 2 * upper_bound / (n - 1);
+  vector<double> X, Y, Y_calc;
+
+  X.reserve(n);
+  Y.reserve(n);
+  Y_calc.reserve(n);
+
+  for (int _ = 0; _ < n; ++_) {
+    double rad = x * M_PI / 180;
+    double sin_h = sin(rad);
+    double cust_sin = custom_sin(x, term_count, true);
+    double error = fabs(sin_h - cust_sin);
+    X.push_back(rad);
+    Y.push_back(sin_h);
+    Y_calc.push_back(cust_sin);
+    fmt::println("{:=^80}", "");
+    fmt::println("{:^10.0f}|{:^20.10f}|{:^25.10f}|{:^30.10f}", x, sin_h, cust_sin, error);
+    x += step;
+  }
+
+  std::stringstream ss;
+  for (int i = 0; i < X.size(); ++i) {
+    double error = fabs(Y_calc[i] - Y[i]);
+    ss << X[i] << ',' << Y_calc[i] << ',' << Y[i] << ',' << error << '\n';
+  }
+
+  std::filesystem::create_directory("data_output/");
+  std::fstream rad("data_output/" + title, std::ios::out | std::ios::trunc);
+  if (!rad) {
+    std::cerr << "Failed to write to the file\n";
+    return;
+  }
+  rad << ss.str();
+  rad.close();
+}
+
+int main() {
+  int n = 100;
+  for (int i = 1; i <= 10; i++) {
+    fmt::println("{:^10}|{:^20}|{:^25}|{:^20}|{:^30}",
+                    "x", "sin_h", "c_sin", "error", "terms: " + std::to_string(i));
+    std::string rad_title{std::to_string(i) + "rad_data.csv"};
+    rad_comparison(n, i, 6 * M_PI, rad_title);
+
+    fmt::print("\n{:=^80}\n\n", "");
+
+    fmt::println("{:^10}|{:^20}|{:^25}|{:^20}|{:^30}",
+                    "x", "sin_h", "c_sin", "error", "terms: " + std::to_string(i));
+    std::string deg_title{std::to_string(i) + "deg_data.csv"};
+    deg_comparison(n, i, 1080, deg_title);
+    printf("\n");
+  }
+  return 0;
+}
---
+++ viz.py
@@ -0,0 +1,53 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_folder = "data_output/"
+chart_folder = "charts/"
+os.makedirs(data_folder, exist_ok=True)
+os.makedirs(chart_folder, exist_ok=True)
+
+for i in range(1, 11):
+    rad_title = data_folder + str(i) + "rad_data.csv"
+
+    try:
+        rad_data = pd.read_csv(rad_title, names=[0, 1, 2, 3], header=None, sep=",")
+        x = rad_data.iloc[:, 0]
+        y_calc = rad_data.iloc[:, 1]
+        y = rad_data.iloc[:, 2]
+        error = rad_data.iloc[:, 3]
+
+        plt.plot(x, y, label="sin_h")
+        plt.plot(x, y_calc, label="custom_sin")
+        plt.plot(x, error, label="error")
+        plt.grid()
+        plt.legend()
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.title(f"Radial Sin Approximation - {i} terms")
+        plt.savefig(chart_folder + "rad_chart" + str(i) + ".png")
+        plt.clf()
+    except FileNotFoundError:
+        print(f"File {rad_title} doesn't exist.")
+
+    deg_title = data_folder + str(i) + "deg_data.csv"
+
+    try:
+        deg_data = pd.read_csv(deg_title, names=[0, 1, 2, 3], header=None, sep=",")
+        x = deg_data.iloc[:, 0]
+        y_calc = deg_data.iloc[:, 1]
+        y = deg_data.iloc[:, 2]
+        error = deg_data.iloc[:, 3]
+
+        plt.plot(x, y, label="sin_h")
+        plt.plot(x, y_calc, label="custom_sin")
+        plt.plot(x, error, label="error")
+        plt.grid()
+        plt.legend()
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.title(f"Degree Sin Approximation - {i} terms")
+        plt.savefig(chart_folder + "deg_chart" + str(i) + ".png")
+        plt.clf()
+    except FileNotFoundError:
+        print(f"File {deg_title} doesn't exist.")
